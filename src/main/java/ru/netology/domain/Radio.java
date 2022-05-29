34cbf52e5aac052c151a2a4b3d02774954d3d79e
Galina Demidova <galinagriz77@gmail.com>
2022-05-29 12:13:32 +0300

    test commit

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,70 @@
 package ru.netology.domain;
 
 public class Radio {
+    private int currentVolume;
+    private int currentNumber;
+
+    public void setCurrentNumber(int currentNumber) {
+        if (currentNumber < 0) {
+            return;
+        }
+        if (currentNumber > 9) {
+            return;
+        }
+        this.currentNumber = currentNumber;
+    }
+
+    public int getCurrentNumber() {
+
+        return currentNumber;
+    }
+
+    public void next() {
+
+        if (currentNumber < 9) {
+            currentNumber = currentNumber + 1;
+        }
+        if (currentNumber == 9) {
+            currentNumber = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentNumber == 0) {
+            currentNumber = 9;
+        }
+        if (currentNumber < 9) {
+            currentNumber = currentNumber - 1;
+        }
+
+    }
+
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
 
 }
+
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,159 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    @Test
+    void soundMax() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(11);
+        int actual = cond.getCurrentVolume();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void soundMin() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(-5);
+        int actual1 = cond.getCurrentVolume();
+        int expected1 = 0;
+        Assertions.assertEquals(expected1, actual1);
+    }
+
+    @Test
+    void sound() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(2);
+        int actual1 = cond.getCurrentVolume();
+        int expected1 = 2;
+        Assertions.assertEquals(expected1, actual1);
+    }
+
+    @Test
+    void numberMax1() {
+        Radio cond = new Radio();
+        cond.setCurrentNumber(10);
+        int actual2 = cond.getCurrentNumber();
+        int expected2 = 0;
+        Assertions.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void numberMin1() {
+        Radio cond = new Radio();
+        cond.setCurrentNumber(-1);
+        int actual2 = cond.getCurrentNumber();
+        int expected2 = 0;
+        Assertions.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void number() {
+        Radio cond = new Radio();
+        cond.setCurrentNumber(3);
+        int actual2 = cond.getCurrentNumber();
+        int expected2 = 3;
+        Assertions.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void numberNext() {
+        Radio cond = new Radio();
+        cond.setCurrentNumber(5);
+
+        cond.next();
+
+        int actual2 = cond.getCurrentNumber();
+        int expected2 = 6;
+        Assertions.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void numberPrev() {
+        Radio cond = new Radio();
+        cond.setCurrentNumber(0);
+
+        cond.prev();
+
+        int actual3 = cond.getCurrentNumber();
+        int expected3 = 9;
+        Assertions.assertEquals(expected3, actual3);
+    }
+
+    @Test
+    void numberNext1() {
+        Radio cond = new Radio();
+        cond.setCurrentNumber(9);
+
+        cond.next();
+
+        int actual2 = cond.getCurrentNumber();
+        int expected2 = 0;
+        Assertions.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void numberPrev2() {
+        Radio cond = new Radio();
+        cond.setCurrentNumber(4);
+
+        cond.prev();
+
+        int actual3 = cond.getCurrentNumber();
+        int expected3 = 3;
+        Assertions.assertEquals(expected3, actual3);
+    }
+
+    @Test
+    void increaseVolume3() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(5);
+
+        cond.increaseVolume();
+
+        int actual2 = cond.getCurrentVolume();
+        int expected2 = 6;
+        Assertions.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void decreaseVolume3() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(3);
+
+        cond.decreaseVolume();
+
+        int actual2 = cond.getCurrentVolume();
+        int expected2 = 2;
+        Assertions.assertEquals(expected2, actual2);
+
+    }
+
+    @Test
+    void increaseVolume4() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(10);
+
+        cond.increaseVolume();
+
+        int actual2 = cond.getCurrentVolume();
+        int expected2 = 10;
+        Assertions.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void decreaseVolume4() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(0);
+
+        cond.decreaseVolume();
+
+        int actual2 = cond.getCurrentVolume();
+        int expected2 = 0;
+        Assertions.assertEquals(expected2, actual2);
+
+    }
+
+}
